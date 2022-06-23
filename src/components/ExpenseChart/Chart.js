c7cf733b690709a429ef64e1beb65139e3aa1d42
Quonn S Bernard <quonn.bernard@gmail.com>
2022-06-23 08:29:20 -0400

    started on expenses chart and chart bar component

---
+++ src/components/ExpenseChart/Chart.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { ChartBar } from './ChartBar';
+import './Chart.css';
+
+export const Chart = props => {
+
+    return(
+        <div>
+            
+        </div>
+    )
+}
---
+++ src/components/ExpenseChart/ChartBar.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import './ChartBar.css';
+
+export const ChartBar = props => {
+
+}
